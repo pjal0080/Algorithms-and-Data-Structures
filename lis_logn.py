239c65b4b866d0b7de2ba820312d55b7da0a8403
pjal0080 <39161176+pjal0080@users.noreply.github.com>
2019-04-14 20:52:59 +0530

    Add files via upload

---
+++ lis_logn.py
@@ -0,0 +1,16 @@
+n = int(input())
+l = [int(i) for i in input().split()]
+ans=[l[0]]
+for i in range(1,n):
+    if(l[i]>ans[-1]):
+        ans.append(l[i])
+    else:
+        x=bl(ans,l[i])
+        if(x>0):
+	    
+            if(l[i]-ans[x-1]>0):	
+                ans[x]=l[i]
+        else:
+            ans[x]=l[i]
+    
+print(len(ans))
