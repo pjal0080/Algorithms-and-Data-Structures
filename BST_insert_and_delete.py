2623fdb347642d35cd771c6dc7141003342aea79
pjal0080 <39161176+pjal0080@users.noreply.github.com>
2022-07-17 13:55:01 +0530

    Update BST_insert_and_delete.py

--- BST_insert_and_delete.py
+++ BST_insert_and_delete.py
@@ -1,95 +1,127 @@
+#Binary Search Tree
+
+
+
 class Node:
+    
     def __init__(self,data):
         self.data = data
-        self.right = None
         self.left = None
-    
-    
-def inordersucc(root):
-    
-    temp = root
-    while(temp.left):
-        temp = temp.left
-    
-    return temp
+        self.right = None
 
-  
-  
-  
-    
+
+
+
 def insert(root,data):
     if(root is None):
         root = Node(data)
         return root
     
+    
+    
     if(root.data >= data):
         root.left = insert(root.left,data)
+       
     
     else:
         root.right = insert(root.right,data)
     
+    
     return root
-     
-    
-    
         
+
+
+
+def inorder(root):
+    if(root is None):
+        return 
+    
+    inorder(root.left)
+    print(root.data)
+    inorder(root.right)
+    
+    
+
+
+def search(root,data):
+    
+    if(root is None):
+        return False
+    
+    if(root.data == data):
+        return True
+    
+    
+    if(root.data > data):
+        return search(root.left,data)
+    
+    else:
+        return search(root.right,data)
+
+
 def delete(root,data):
     
     if(root is None):
         return root
-    
     
     if(root.data == data):
         if(root.left is None and root.right is None):
             root = None
             return root
         
-        elif(root.right is None):
-            temp = root.left
+        elif(root.left and root.right is None):
+            node = root.left
             root = None
-            return temp
+            return node
         
-        
-        elif(root.left is None):
-            temp = root.right
+        elif(root.right and root.left is None):
+            node = root.right
             root = None
-            return temp
+            return node
         
         else:
-            
-            x = inordersucc(root.right)
-            root.data = x.data
-            
-            root.right = delete(root.right,x.data)
-            
+            node = successor(root.right)
+            root.data = node.data
+            root.right = delete(root.right,node.data)
     
-    elif(root.data >= data):
+    
+    if(root.data > data):
         root.left = delete(root.left,data)
+    
     
     else:
         root.right = delete(root.right,data)
-            
         
-    return root    
-     
     
+    return root
     
+
+def successor(root):
     
-def inorder(root):
-    if(root is None):
-        return
+    temp = root
+    while(temp.left):
+        temp = temp.left
     
-    inorder(root.left)
-    print(root.data)
-    inorder(root.right)
-    
-root = None
-root = insert(root,12)
-root = insert(root,14)
-root = insert(root,16)
-root = insert(root,10)
-root = insert(root,8)
-root = insert(root,9)
+    return temp    
+
+
+
+
+
+root = insert(None,10)
+root = insert(root,20)
+root = insert(root,30)
+root = insert(root,5)
+root = insert(root,7)
+root = insert(root,15)
+
 inorder(root)
-root = delete(root,10)
+
+print(search(root,2))
+print(search(root,7))
+print(search(root,30))
+print(search(root,15))
+
+
+root = delete(root,20)
 inorder(root)
